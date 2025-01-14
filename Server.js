498f6d8e049ea4887c84a53addc21744c5727891
SaraCohen3446 <shhryk0659@gmail.com>
2025-01-14 17:27:00 +0200

    last

--- Server.js
+++ Server.js
@@ -2,11 +2,12 @@ import express from "express";
 import userRouter from "./routers/user.js"
 import productRouter from "./routers/product.js"
 import orderRouter from "./routers/order.js"
-import {connectToDb} from  "./config/DB.js"
+import { connectToDb } from "./Config/db.js";
 
 import dotenv  from "dotenv"
 import { connect } from "mongoose";
-import cors from "cors"
+import cors from 'cors';
+
 
 dotenv.config();
 const app = express();
@@ -23,40 +24,3 @@ app.listen(port, "0.0.0.0", () => {
 
 
 
-// import express from "express"
-// import userRouter from "./routers/user.js";
-// import { connectToDb } from "./config/DB.js";
-// import dotenv from "dotenv"
-// import fs from "fs/promises";
-// import cors from "cors"
-// async function PrintToLog(req, res, next) {
-
-//     try {
-    
-//     await fs.appendFile("./log.txt", ` ${new Date().toLocaleDateString()} ${req.method} ${req.url}`);
-    
-//     next();
-    
-//     } catch (err) {
-    
-//     return res.status(400).json({ title: "error in print to Log", message: err.message });
-    
-//     }
-    
-//     }
-
-// dotenv.config()
-// const app = express();
-// connectToDb()
-// app.use(PrintToLog)
-// app.use(cors())
-// app.use(express.json())
-// app.use("/api/product", );
-// app.use("/api/user", userRouter);
-
-// let port = process.env.PORT;
-
-// app.listen(port, "localhost", () => {
-//     app.listen(port, "0.0.0.0", () => {
-//     console.log("app is listening on port " + port)
-// })
